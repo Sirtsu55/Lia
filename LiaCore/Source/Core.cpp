938953b296b7af07ec618e6c243f226609be8001
Srijan Dhungana <srijan.dhungana6@gmail.com>
2022-08-19 08:45:41 +0300

    Diligent Core running

--- LiaCore/Source/Core.cpp
+++ LiaCore/Source/Core.cpp
@@ -1,15 +1,37 @@
 #include "Window.h"
+#include "EngineFactoryVk.h"
+
+//dg = Diligent Graphics
+namespace dg = Diligent;
 
 
 int main()
 {
 	Log::Init();
     Lia::Window::Init();
+
+    dg::IRenderDevice*  m_pDevice;
+    dg::IDeviceContext* m_pImmediateContext;
+    dg::ISwapChain*     m_pSwapChain;
+    dg::IPipelineState* m_pPSO;
+    dg::EngineVkCreateInfo EngineCI;
+
+    auto* pFactoryVk = dg::GetEngineFactoryVk();
+    pFactoryVk->CreateDeviceAndContextsVk(EngineCI, &m_pDevice, &m_pImmediateContext);
+    dg::SwapChainDesc     SCDesc;
+
+
+
+
     {
         Lia::Window::Settings settings = { .Name = "LiaEngine", .Resolution = glm::ivec2(1280, 720) };
         
         Sptr<Lia::Window> win = CreateSptr<Lia::Window>(settings);
+
+        dg::NativeWindow nativeWin;
         
+        nativeWin.hWnd = glfwGetWin32Window(win->GetGLFWWindow());
+        pFactoryVk->CreateSwapChainVk(m_pDevice, m_pImmediateContext, SCDesc, nativeWin, &m_pSwapChain);
         
         while (win->IsActive())
         {
---
+++ LiaCore/Source/Graphics/ComputeTracer.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Tracer.h"
+
+namespace Lia
+{
+	class ComputeTracer : public Tracer
+	{
+	public:
+		ComputeTracer();
+		~ComputeTracer();
+	private:
+
+
+
+	};
+
+}
+
